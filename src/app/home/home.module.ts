7ddf962004fc3b05c3bd9487c1485abcaff85b75
Damian Tarnawsky <damian@ionic.io>
2022-04-07 10:48:21 -0700

    Initial Commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { Drivers } from '@ionic/storage';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
@@ -12,8 +14,11 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    IonicStorageModule.forRoot({
+      driverOrder: [Drivers.SecureStorage, Drivers.IndexedDB, Drivers.LocalStorage]
+    })
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Storage } from '@ionic/storage-angular';
+import IonicSecureStorageDriver from '@ionic-enterprise/secure-storage/driver';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private storage: Storage) { }
+
+  async ngOnInit() {
+    await this.storage.defineDriver(IonicSecureStorageDriver);
+    await this.storage.setEncryptionKey('secret');
+    await this.storage.create();
+  }
+
+  async test() {
+    await this.storage.set('blar', 'test');
+  }
+
+  async fail() {
+    try {
+      await this.storage.defineDriver(IonicSecureStorageDriver);
+      await this.storage.setEncryptionKey('wrongsecret');
+      await this.storage.create();
+    } catch (err) {
+      alert('Got an expected error creating storage: ' + err);
+    }
+    try {
+      await this.storage.get('blar');
+      alert('Shouldnt happen');
+    } catch (err) {
+      // We should get to here because the get fails
+      alert('Got an expected error: ' + err);
+    }
+  }
+
 
 }
